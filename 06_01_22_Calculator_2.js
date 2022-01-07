07482b42b1bc795eb3b4f0859fd1dbeda6813fda
Thiet Nguyen <nguyenthiet1225@gmail.com>
2022-01-07 08:16:42 +0700

    new pj

---
+++ 06_01_22_Calculator_2.js
@@ -0,0 +1,10 @@
+
+function i1() {
+    let x = Number(document.getElementById("screen").value);
+    y = x + 1;
+    document.getElementById("screen").innerText = y;
+}
+function i2() {
+    let x = Number(document.getElementById("screen").value);
+    document.getElementById("screen").innerHTML = 2
+}
